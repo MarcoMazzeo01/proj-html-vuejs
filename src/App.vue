dda1f8636b17eeb4b8311037e53b73c609839dbb
plastikSun <marcom2k01@gmail.com>
2023-09-20 19:22:18 +0200

    add: floating bottom (go up and support messages)

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,18 @@ export default {
   <Header />
   <Main />
   <Footer :footerMenu="footerMenu" />
+
+  <div class="floating_buttons">
+    <button type="button" class="go-up">
+      <a href="#header"
+        ><font-awesome-icon icon="fa-solid fa-arrow-up-long " size="xs"
+      /></a>
+    </button>
+
+    <button type="button" class="support">
+      <font-awesome-icon icon="fa-solid fa-comment" />
+    </button>
+  </div>
 </template>
 
 <style lang="scss">
@@ -25,6 +37,36 @@ export default {
 
 p {
   font-weight: 300;
+}
+
+.floating_buttons {
+  position: fixed;
+  z-index: 666;
+  bottom: 10px;
+  right: 10px;
+
+  button {
+    aspect-ratio: 1;
+    border: 0;
+    border-radius: 100%;
+    margin: 0 5px;
+  }
+
+  .go-up {
+    width: 25px;
+    background-color: $opaque-white;
+
+    a {
+      color: $black;
+      text-decoration: none;
+    }
+  }
+
+  .support {
+    width: 45px;
+    color: $opaque-white;
+    background-color: $navy-blue;
+  }
 }
 
 // ! SCROLLBAR ! //
